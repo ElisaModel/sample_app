1fdecad81ce891bd5c1ad2f36ae9221845dad3d3
Liza <braun.yelyzaveta.dev@gmail.com>
2023-06-02 13:02:26 +0000

    Add account activation

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -14,7 +14,7 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
-  #Returns the user corresponding to the remember token cookie.
+  # Returns the current logged-in user (if any).
   def current_user
     if (user_id = session[:user_id])
       user = User.find_by(id: user_id)
@@ -23,7 +23,7 @@ module SessionsHelper
       end
     elsif (user_id = cookies.encrypted[:user_id])
         user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
+      if user && user.authenticated?(:remember,cookies[:remember_token])
         log_in user
         @current_user = user
       end
@@ -53,7 +53,7 @@ module SessionsHelper
     reset_session
     @current_user = nil
   end
-  
+
   # Stores the URL trying to be accessed.
   def store_location
     session[:forwarding_url] = request.original_url if request.get?
