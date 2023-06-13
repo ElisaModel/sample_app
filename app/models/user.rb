899d41f38d8df6b40d6b94d1acb1261d2085fbd8
Liza <braun.yelyzaveta.dev@gmail.com>
2023-06-13 09:18:16 +0000

    Add user microposts2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,19 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  VALID_EMAIL_REGAX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
+
   before_save :downcase_email
   before_create :create_activation_digest
-    validates :name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGAX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
-  format: { with: VALID_EMAIL_REGAX },
-  uniqueness: true
-  has_secure_password
+
+  validates :name, presence: true, length: { maximum: 50 }
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  validates :email, presence: true, length: { maximum: 255 },
+    format: { with: VALID_EMAIL_REGAX }, uniqueness: true
+
 
   class << self
 
@@ -75,6 +80,12 @@ class User < ApplicationRecord
   # Returns true if a password reset has expired.
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
+  end
+
+  # Defines a proto-feed.
+  # See "Following users" for the full implemention.
+  def feed
+    Micropost.where("user_id = ?", id)
   end
 
   private
